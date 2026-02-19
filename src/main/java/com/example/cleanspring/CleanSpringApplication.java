0ef84c7f5637a1cacf26bd88b682a4f3bc02cc6e
wookey(안형욱) <wookey.ahn@bunjang.co.kr>
2026-02-19 19:41:32 +0900

    feat(WP-3): A추가

--- src/main/java/com/example/cleanspring/CleanSpringApplication.java
+++ src/main/java/com/example/cleanspring/CleanSpringApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CleanSpringApplication {
 
     public static void main(String[] args) {
+        System.out.println("A");
         SpringApplication.run(CleanSpringApplication.class, args);
     }
 
