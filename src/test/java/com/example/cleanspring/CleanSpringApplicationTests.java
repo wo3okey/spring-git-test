3b1c1c5b26ff4f9bad16b3e214b93d027519191a
wookey(안형욱) <wookey.ahn@bunjang.co.kr>
2026-02-19 17:53:22 +0900

    init

---
+++ src/test/java/com/example/cleanspring/CleanSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cleanspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
