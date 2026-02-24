9cd59f8495968239970b87c76bd653ba7e13b55d
wookey(안형욱) <wookey.ahn@bunjang.co.kr>
2026-02-24 19:14:35 +0900

    feat(WP-6): 어떤 코드 나도 작성함

--- src/main/java/com/example/cleanspring/CleanSpringApplication.java
+++ src/main/java/com/example/cleanspring/CleanSpringApplication.java
@@ -8,6 +8,7 @@ public class CleanSpringApplication {
 
     public static void main(String[] args) {
         System.out.println("A");
+        System.out.println("나도 어떤 작업함");
         SpringApplication.run(CleanSpringApplication.class, args);
     }
 
